5e22e90ef893e90027196b9ba0b908867ac7d14f
Popovskii Oleksii <pad19051999@gmail.com>
2021-12-01 18:29:13 +0200

    Add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,39 @@ import DecrementButton from './components/DecrementButton';
 import StepHandler from './components/StepHandler';
 import { ICounter } from './interfaces/counter';
 import Form from "./components/Form";
+import {Link, Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 
 const counter: ICounter = new Counter();
 
 const App: React.FC = () => {
   return <>
-    <div>App was loaded...</div>
-    <CountView counter={counter}/>
-    <IncrementButton counter={counter}/>
-    <DecrementButton counter={counter}/>
-    <StepHandler counter={counter}/>
-    <Form/>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/counter">Counter</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/counter" element={<>
+            <CountView counter={counter}/>
+            <IncrementButton counter={counter}/>
+            <DecrementButton counter={counter}/>
+            <StepHandler counter={counter}/></>} />
+          <Route path="/users" element={<Form/>} />
+          <Route path="/" element={<div>App was loaded...</div>} />
+        </Routes>
+      </div>
+    </Router>
   </>;
 };
 
